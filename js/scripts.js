439a161a3678adf736238449e0fa9b94d319e551
jvinge <justin@deviine.com>
2016-05-17 12:27:44 -0700

    major updates to website

--- js/scripts.js
+++ js/scripts.js
@@ -1,123 +1,133 @@
-//THIS IS MY SCRIPTS.JS
+// //Returns true if the specified element has been scrolled into the viewport.
+// function isElementInViewport(elem) {
+//     var $elem = $('path');
 
-//THIS IS A MODULE
-// @todo create an angular module that launches a whoAreYou modal. 
-// @todo create an angular module that accepts the user's name and adds it to firebase.
-// @todo create an angular module that gets user's location.
-// @todo create an angular module that conisders the user's location and selects the nearest agency.
-// @todo create an angular module that asks for the user to confirm their agency.
-var $document = $(document),
-    $element = $('#header'),
-    className = 'hide-bg';
+//     // Get the scroll position of the page.
+//     var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+//     var viewportTop = $(scrollElem).scrollTop();
+//     var viewportBottom = viewportTop + $(window).height();
 
-// $(document).ready(function() {
-//     $(".divider").animate({width: "+=50%"}, 2000);
-//     $(".divider").animate({width: "-=50%"}, 2000);
-// setTimeout(arguments.callee, 1000);
+//     // Get the position of the element on the page.
+//     var elemTop = Math.round( $elem.offset().top );
+//     var elemBottom = elemTop + $elem.height();
+
+//     return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+// }
+
+// // Check if it's time to start the animation.
+// function checkAnimation() {
+//     var $elem = $('.line-animated');
+//     console.log('shitty scroll works');
+//     // If the animation has already been started
+//     if ($elem.hasClass('start')) return;
+// console.log('has class');
+//     if (isElementInViewport($elem)) {
+//         // Start the animation
+//         $elem.addClass('start');
+//         console.log('added class');
+//     }
+// }
+
+// // Capture scroll events
+// $(window).scroll(function(){
+//     checkAnimation();
 // });
 
-$document.scroll(function() {
-//   $element.toggleClass(className, $document.scrollTop() >= 50);
+
+
+
+// $(document).ready(function () {
+// var $line = $('.line-animated');
+// var firstScroll = false;
+// $(".line-animated").css("opacity", "0");
+
+// $(window).on('scroll', function() {
+//   console.log('scroll works');
+//   var scroll = $(window).scrollTop();
+//   if(scroll >= $('#nes').offset().top && !firstScroll) {
+//     doSomeComplicatedStuff();
+//   }
 // });
-// $('#name').hide();
-// $(document).ready(function() {
-// 	 var j = 0;
-// 	 var delay = 3500; //millisecond delay between cycles
-// 	 function cycleThru(){
-// 	         var jmax = $("ul#line3 li").length -1;
-// 	         $("ul#line3 li:eq(" + j + ")")
-// 	                 .animate({"opacity" : "1"} ,400)
-// 	                 .animate({"opacity" : "1"}, delay)
-// 	                 .animate({"opacity" : "0"}, 400, function(){
-// 	                         (j == jmax) ? j=0 : j++;
-// 	                         cycleThru();
-// 	                 });
-// 	         };
+// function doSomeComplicatedStuff() {
+//   console.log('doing complicated stuff');
+//   firstScroll = true;        
+//   $(".line-animated").css("strokeDashoffset", "988.01");
+//   //Reset paths to starting position
+//   lineDraw();
+// }
+// function lineDraw(){
+// var length = $(".line-animated").getTotalLength();
+// // Clear any previous transition
+// $line.style.transition = $line.style.WebkitTransition =
+//   'none';
 
-// 	 cycleThru();
+// // Set up the starting positions
+// $line.style.strokeDasharray = length + ' ' + length; 
+// $line.style.strokeDashoffset = length;
+// // Trigger a layout so styles are calculated & the browser 
+// // picks up the starting position before animating
+// $line.getBoundingClientRect();
+// // Define our transition
+// $line.style.transition = $line.style.WebkitTransition =
+//   'stroke-dashoffset 7s ease-in-out';
+//   'opacity 7s ease-in-out';
+// // Go!
+// $line.style.strokeDashoffset = '0';
+// $line.style.opacity = '1';
+//   //0 is the image fully animated, 988.01 is the starting point.
+// };
+// });
 
-		  //For Firefox users
-	 $('body').bind('DOMMouseScroll', function(e){
-	     if(e.originalEvent.detail > 0) {
-	         //scroll down
-	         console.log('Down');
-	         //hide my logo on scroll down
-	         $( "#lion" ).fadeOut(100);
-	         $( "#name" ).fadeIn(100);
-	     }else if(e.originalEvent.detail < 0) {
-	         //scroll up
-	         console.log('Up');
-	         $( "#name" ).fadeOut(100);
-	         $( "#lion" ).fadeIn(100);
-	     }
+// Caching jQuery selectors: another micro-optimization / protip.
+// @see https://github.com/airbnb/javascript#25.2
+var $window = $(window);
+//on a scroll event - execute function
+$window.scroll(function() {
+  var scroll = $window.scrollTop();
+  var $image = $('#image');
+  var $logo = $('#logo-jvd');
+  var $toplaunch = $('#top-launch');
+  var $navbuttons = $('#nav-buttons');
+  var $results = $('#results');
 
-	     //prevent page fom scrolling
-	     return true;
-	 });
+  // @see http://api.jquery.com/offset/
+  if(scroll >= $('#restricted').offset().top) {
+    $image.addClass('fix-spokane');
+    $image.css('transform', '');
+    $logo.addClass('logo-down');
+    $logo.removeClass('logo-up');
+    $toplaunch.addClass('top-launch-visible');
+    $toplaunch.addClass('top-launch-animate');
+    $navbuttons.css("z-index", "6");
+  } else {
+    $image.removeClass('fix-spokane');
+    $image.css("transform","translateY(" +  (scroll/2)  + "px)");
+    $logo.addClass('logo-up');
+    $logo.removeClass('logo-down');
+    $toplaunch.removeClass('top-launch-animate');
+    $toplaunch.removeClass('top-launch-visible');
+    $navbuttons.css("z-index", "-3");
+  }
+});
+    /* If the object is completely visible in the window, fade it in */
+$window.scroll(function() { 
+  var $path = $('.lineogo');
+  var bottom_of_object = $path.offset().top + $path.outerHeight();
+  var bottom_of_window = $(window).scrollTop() + $(window).height(); 
+  if( bottom_of_window > bottom_of_object ){
+    $('.lineogo').attr("class", "start");
+    console.log('line animated!');
+  }
+});
 
-	 // For IE, Opera, Safari, and Chrome users
-	 $('body').bind('mousewheel', function(e){
-	     if(e.originalEvent.wheelDelta < 0) {
-	         //scroll down
-	         console.log('Down');
-	         //hide my logo on scroll down
-	         $( "#lion" ).fadeOut('fast', function() {
-			    // will be called when the element finishes fading out
-			    // if selector matches multiple elements it will be called once for each
-			    $( "#name" ).fadeIn('fast');
-			});
-	     }else if(e.originalEvent.wheelDelta > 0) {
-	         //scroll up
-	         console.log('Up');
-	         //hide my logo on scroll down
-	         $( "#name" ).fadeOut('fast', function() {
-			    // will be called when the element finishes fading out
-			    // if selector matches multiple elements it will be called once for each
-			    $( "#lion" ).fadeIn('fast');
-			});
-	     }
-
-	     //prevent page fom scrolling
-	     return true;
-	 });
-  var percentage1 = $('.flav1').attr('data-value');
-  var percentage2 = $('.flav2').attr('data-value');
-  var percentage3 = $('.flav3').attr('data-value');
-	var chart = new CanvasJS.Chart("flavorChart", {
-    animationEnabled: true,     
-		data: [
-		{        
-			type: "doughnut",
-			startAngle: 60,                					
-			showInLegend: false,
-			dataPoints: [
-				{y: percentage1/*, indexLabel: "EXAMPLE TITLE #percent%"*/},
-				{y: percentage2},
-				{y: percentage3}			
-			]
-		}
-		]
-	});
-	chart.render();
-	// on page load...
-    moveProgressBar();
-    // on browser resize...
-    $(window).resize(function() {
-        moveProgressBar();
-    });
-
-    // SIGNATURE PROGRESS
-    function moveProgressBar() {
-      console.log("moveProgressBar");
-        var getPercent = ($('.progress-wrap').data('progress-percent') / 5);
-        var getProgressWrapWidth = $('.progress-wrap').width();
-        var progressTotal = getPercent * getProgressWrapWidth;
-        var animationLength = 1700;
-        
-        // on page load, animate percentage bar to data percentage length
-        // .stop() used to prevent animation queueing
-        $('.progress-bar').stop().animate({
-            left: progressTotal
-        }, animationLength);
-    }
- });
+$(function () {
+    $(window).scroll(function () {
+        var y = $(window).scrollTop();
+        var r = $('#results').offset().top;
+        $('#results').height(y);
+        x = y - 300;
+        $myDiv.animate({
+            height: x
+        }, 250);
+    }).scroll();
+});
